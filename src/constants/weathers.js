f3e8d1b468cd74cdd7ce13d609ed0bb8c97f3d5c
eugeniokrupa <carrieguito@gmail.com>
2018-10-19 09:31:30 -0300

    19-10

--- src/constants/weathers.js
+++ src/constants/weathers.js
@@ -1,4 +1,5 @@
 export const CLOUD = "cloud";
+<<<<<<< HEAD
 
 export const CLOUDY = "cloudy";
 
@@ -9,3 +10,10 @@ export const RAIN = "rain";
 export const SNOW = "snow";
 
 export const WINDY = "windy";
+=======
+export const CLOUDY = "cloudy";
+export const SUN = "sun";
+export const RAIN = "rain";
+export const SNOW = "snow";
+export const WINDY= "windy"
+>>>>>>> b0a2ee5ad0dbe4984af47ee0d1be7664eb324544
